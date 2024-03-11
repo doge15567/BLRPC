0f0a410909788e88d07f84b63cbf4e0092245da4
doge15567 <41397214+doge15567@users.noreply.github.com>
2024-03-10 21:22:07 -0500

    First public release
    Untested

---
+++ Presence/Handlers/RemoteImageHandler.cs
@@ -0,0 +1,79 @@
+﻿using BLRPC.Melon;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using ModioModNetworker;
+using ModioModNetworker.Utilities;
+using ModioModNetworker.Data;
+using SLZ.Marrow.Warehouse;
+
+namespace BLRPC.Internal
+{
+    internal class RemoteImageHandler
+    {
+
+        public static Dictionary<string, string> ImageCache;
+
+
+        public static string CheckforExternalImage(string ImageKey, string FallBackKey)
+        {
+
+            ModConsole.Msg("Check External Image Called with value " + ImageKey, 1);
+
+            if (ImageCache.ContainsKey(ImageKey))
+            {
+                ModConsole.Msg(ImageKey + " was already processed, and was cached with value " + ImageCache[ImageKey], 1);
+                return ImageCache[ImageKey];
+            }
+
+            string modiofallbackuri = GetModioImage(ImageKey, FallBackKey);
+            if (modiofallbackuri != null) { ImageCache[ImageKey] = modiofallbackuri; }
+            else { ImageCache[ImageKey] = FallBackKey; }
+
+            ModConsole.Msg("Image Key Cache of " + ImageKey + " was set with " + ImageCache[ImageKey], 1);
+
+            ModConsole.Msg("Check for external image"+ImageKey+" finished, with value " + ImageCache[ImageKey], 1);
+            return ImageCache[ImageKey];
+        }
+
+        public static ModInfo GetModInfoForAvatarBarcode(string barcode) // Unimplemented in ModioModNetworker for some reason?????
+        {
+            AvatarCrate avatarCrate =
+                AssetWarehouse.Instance.GetCrate<AvatarCrate>(barcode);
+            if (avatarCrate == null)
+            {
+                return null;
+            }
+
+            string palletBarcode = avatarCrate._pallet._barcode;
+            return ModInfoUtilities.GetModInfoForPalletBarcode(palletBarcode);
+        }
+
+
+
+
+        public static string GetModioImage(string imageKey, string FallbackKey)
+        {
+            ModInfo info;
+            if (FallbackKey == "moddedmap") { info = ModInfoUtilities.GetModInfoForLevelBarcode(imageKey); }
+            else if (FallbackKey == "moddedavatar") { info = GetModInfoForAvatarBarcode(imageKey); }
+            else return null;
+            if (info.IsTracked())
+            {
+                ModConsole.Msg("Mod was trackde and we returned " + info.thumbnailLink, 1);
+                return info.thumbnailLink;
+            }
+            else return null;
+        }
+
+    }
+
+}
+
+
